cdf6ca65e9dabf0bc2ef4a30ef81f90ab42eae82
Basstrwp <113402668+Basstrwp@users.noreply.github.com>
2022-10-09 20:42:58 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -359,7 +359,7 @@ let tek = `✧────···[ Dashboard ]···────✧
 │
 ├━━━━━━━━━━━━━━━━┈─⋆
 │
-│ ▸ *Sumber :* YouTube WH-MODS-DEV
+│ ▸ *Sumber :* YouTube Kyluesky
 │ ▸ *ᴀᴜᴛʜᴏʀ :* ${nameown}
 ┴ ▸ *ᴏᴡɴᴇʀ :* ${nameown}
 ✧
